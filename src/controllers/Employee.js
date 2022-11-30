15dc7b4b37be0e814af825cb8658774b183e4751
Oluwasinmisola Akinrele <akinrelesimisola@yahoo.com>
2022-11-29 20:02:20 -0500

    validate params

--- src/controllers/Employee.js
+++ src/controllers/Employee.js
@@ -1,8 +1,11 @@
 import Model from '../database/Employee';
+import {validateParams} from '../utils/databaseTools';
+
+const TABLE_NAME = 'employees';
 
 const Employee = {
   index: (req, res) => {
-    Model.all('employees').then((response) =>{
+    Model.all(TABLE_NAME).then((response) =>{
       res.status(200).send(response);
     }).catch((error) => {
       res.status(400).send({message: error.message});
@@ -10,22 +13,26 @@ const Employee = {
   },
 
   create: (req, res) => {
-    const tableName = 'employees';
-    Model.where(tableName, {search: {email: req.body.email}}).then(response => {
-      if (response.length > 0) {
-        return res.status(409).send({ message: `There is a user with this email: ${req.body.email}` });
-      }
+    console.log(req.body, 'body')
+    if (validateParams(req.body)) {
+      Model.where(TABLE_NAME, {search: {email: req.body.email}}).then(response => {
+        if (response.length > 0) {
+          return res.status(409).send({ message: `There is a user with this email: ${req.body.email}` });
+        }
 
-      Model.insert(tableName, req.body).then((response) => {
-        res.status(201).send(response)
-      }).catch((error) => {
-        res.status(400).send({message: error.message});
+        Model.insert(TABLE_NAME, req.body).then((response) => {
+          res.status(201).send(response)
+        }).catch((error) => {
+          res.status(400).send({message: error.message});
+        })
       })
-    })
+    } else {
+      res.status(400).send({message: 'Check that all params are present'})
+    }
   },
 
   show: (req, res) => {
-    Model.findOne('employees', req.params.id).then((response) => {
+    Model.findOne(TABLE_NAME, req.params.id).then((response) => {
       res.status(200).send(response)
     }).catch((error) => {
       res.status(404).send({message: error.message});
@@ -37,7 +44,7 @@ const Employee = {
       req.body = {sortBy: ['firstName', 'lastName']};
     }
 
-    Model.where('employees', req.body).then((response) => {
+    Model.where(TABLE_NAME, req.body).then((response) => {
       res.status(200).send(response)
     }).catch((error) => {
       res.status(404).send({message: error.message});
--- src/utils/databaseTools.js
+++ src/utils/databaseTools.js
@@ -5,3 +5,9 @@ export const sortRecords = (records, sortKeys) => {
   return sorted;
 }
 
+export const validateParams = (params) => {
+  const validKeys = ['email', 'firstName', 'lastName', 'department', 'salary', 'status'];
+  const isValidKeys = Object.keys(params).every(key => validKeys.includes(key));
+  const isNotEmptyValues = Object.values(params).every(value => ![null, '', undefined].includes(value));
+  return isValidKeys && isNotEmptyValues;
+}
